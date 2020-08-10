09494f9e4bacf199a9971f02a5fa47c20da385ea
sfespinosa <scottjames.espinosa@gmail.com>
2020-08-10 15:05:19 -0700

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,68 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    displayedStocks: [],
+    myPortfolio: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(json => this.setState({
+      stocks: json,
+      displayedStocks: json
+    }))
+  }
+
+  addToPortfolio = stock => {
+    let myPortfolio = [...this.state.myPortfolio]
+    if (!myPortfolio.includes(stock)) {
+      myPortfolio.push(stock)
+      this.setState({ myPortfolio })
+    }
+  }
+
+  removeFromPortfolio = selectedStock => {
+    let myPortfolio = this.state.myPortfolio.filter(stock => stock !== selectedStock)
+    this.setState({ myPortfolio })
+  }
+
+  handleSortChange = e => {
+    if (e.target.value === 'Price') {
+      let displayedStocks = this.state.displayedStocks.sort((a,b) => (a.price - b.price))
+      this.setState({ displayedStocks })
+    } else if (e.target.value === 'Alphabetically') {
+      let displayedStocks = this.state.displayedStocks.sort((a,b) => (a.name > b.name ? 1 : -1))
+      this.setState({ displayedStocks })
+    }
+  }
+
+  handleFilterChange = e => {
+    if (e.target.value === 'All') {
+      let displayedStocks = [...this.state.stocks]
+      this.setState({ displayedStocks })
+    } else {
+      let displayedStocks = this.state.stocks.filter(stock => stock.type === e.target.value)
+      this.setState({ displayedStocks })
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleSortChange={this.handleSortChange} handleFilterChange={this.handleFilterChange}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer displayedStocks={this.state.displayedStocks} addToPortfolio={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer myPortfolio={this.state.myPortfolio} removeFromPortfolio={this.removeFromPortfolio}/>
 
             </div>
           </div>
