1643b7eb205624bb5b7733ece95906551a5bde23
iona-b <ionabrabender@gmail.com>
2020-08-10 18:12:38 -0700

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,24 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
+  
+  return(
+      <div>
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+        <div className="card" onClick={(event) => props.addOrRemoveStock(props.stock, event)}>
+          <div className="card-body">
+            <h5 className="card-title">{
+                props.stock.name
+              }</h5>
+            <p className="card-text">{
+                props.stock.price
+              }</p>
+          </div>
+        </div>
+
+
       </div>
-    </div>
-
-
-  </div>
-);
+  )
+};
 
 export default Stock
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,11 +4,14 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+    // debugger
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+           this.props.portfolio.map (stock => {
+            return <Stock stock={stock} stockId={stock.id} addOrRemoveStock={this.props.removeStock}/>
+           })
           }
       </div>
     );
